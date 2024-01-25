f9a0cb552cdf0a7e63b9e374588a6c2e38c62168
Gaurav Pachbhai <104085687+GauravPachbhai@users.noreply.github.com>
2024-01-25 10:25:31 +0000

    25jan

---
+++ basic/DateTime.js
@@ -0,0 +1,17 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString());
+
+let Date1 = Date.now();
+// console.log(Date1);
+// console.log(myDate.getTime());
+
+
+let Date2 = new Date();
+console.log(Date2.getMonth()+1);
+console.log(Date2.getDate());
+console.log(Date2.getDay());
+console.log(Date2.getFullYear());
+
+
